c322de65e5006b1cd437a30cece1509b5d2dbab7
Qioll <ksakharov.nick@gmail.com>
2023-01-03 23:48:32 +0300

    Task_2

---
+++ Task_2/Task_2.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int first,second;
+    printf("Введите первое число\n Первое число = ");
+    scanf("%i",&first);
+    printf("Введите второе число\n Второе число = ");
+    scanf("%i",&second);
+    if (first == second)
+    {
+        printf("Равны\n");
+    }
+    else
+    {
+        if (first > second)
+        {
+            printf("Больше\n");
+        }
+        else
+        {
+            printf("Меньше\n");
+        }
+    }
+    system("pause");
+}
