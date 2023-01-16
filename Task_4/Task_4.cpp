5703bf4b146cd8defa91f97853e5a8203c5e5256
Qioll <ksakharov.nick@gmail.com>
2023-01-16 12:50:48 +0300

    Task_4

---
+++ Task_4/Task_4.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale (LC_ALL, "Russian");
+    int number; 
+    scanf_s("%i",& number);
+
+    switch (number) {
+    case 1:
+        printf ("Понедельник\n");
+        break;
+    case 2:
+        printf("Вторник\n");
+        break;
+    case 3:
+        printf("Среда\n");
+        break;
+    case 4:
+        printf("Четверг\n");
+        break;
+    case 5:
+        printf("Пятница\n");
+        break;
+    case 6:
+        printf("Суббота\n");
+        break;
+    case 7:
+        printf("Воскресенье\n");
+        break;
+    default:
+        printf("Error\n");
+    }
+    system("pause");
+    return 0;
+    
+}
+
+
