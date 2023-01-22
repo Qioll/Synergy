3e24840884074f7aa78cc2f1dd17abac85b256a6
Qioll <ksakharov.nick@gmail.com>
2023-01-22 04:00:39 +0300

    Task_14

---
+++ Task_14/Task_14.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int row_length, column_length;
+
+    printf("Введите количество строк: ");
+    scanf_s("%i", &row_length);
+    printf("Введите количество столбцов: ");
+    scanf_s("%i", &column_length);
+
+    vector<vector<int>> arr(row_length, vector<int>(column_length));
+
+    for (int i = 0; i < row_length; i++)
+    {
+        for (int j = 0; j < column_length; j++)
+        {
+            arr[i][j] = -2 + (rand() % 10);
+        }
+    }
+
+    printf("\nВывод массива:\n");
+    for (int i = 0; i < row_length; i++)
+    {
+        for (int j = 0; j < column_length; j++)
+        {
+            printf("%2i", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    system("pause");
+    return 0;
+}
