8ea66eb1115ce768e808e9e2622c1ae87b4e4309
Qioll <ksakharov.nick@gmail.com>
2023-02-01 16:39:43 +0300

    Create Task_17.cpp

---
+++ Task_17/Task_17.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    char arr[100];
+    int char_number = 0;
+    printf("Введите слово: ");
+    gets_s(arr);
+    printf("2-й символ: %c\n", arr[1]);
+    int arrSize = strlen(arr) ;
+    printf("Последний символ: %c\n", arr[arrSize-1]);
+    swap(arr[0], arr[1]);
+    printf("Поменяли местами 1-й и 2-й символы местами: %s\n", arr);
+
+    printf("\nВведите номер элемента строки, который нужно удалить: ");
+    scanf_s("%i", &char_number);
+    for (int i = char_number - 1;i < arrSize;i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+    printf("Строка после удаления элемента: %s\n", arr);
+    system("pause");
+}
