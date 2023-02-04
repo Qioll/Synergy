75f32ba9e04e92d32d56f206b9511a8f4a576e97
Qioll <ksakharov.nick@gmail.com>
2023-02-04 14:21:33 +0300

    Task_19

---
+++ Task_19/Task_19.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+
+void fill(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 10;
+    }
+
+    printf("\nЗаполненый массив: \n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%2i  ", arr[i]);
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(NULL));
+    int arr_first[25];
+    int arr_second[50];
+    fill(arr_first, 25);
+    fill(arr_second, 50);
+
+    printf("\n");
+    system("pause");
+    return 0;
+}
