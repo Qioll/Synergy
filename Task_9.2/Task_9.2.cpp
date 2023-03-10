8f5e45cbc7b19c55102e03df3ebf3170aa2b2bac
Qioll <ksakharov.nick@gmail.com>
2023-03-10 13:55:34 +0300

    Task_9.2

---
+++ Task_9.2/Task_9.2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	system("chcp 1251");
+
+	string mas[10];
+	for (int i = 0; i < 5; i++)
+	{
+			cin >> mas[i];
+	}
+
+	for (int i = 0; i < 4; i++)
+	{ 
+		for (int j = 0; j < 4; j++)
+		{
+			if (mas[j][0] > mas[j + 1][0])
+				swap(mas[j], mas[j + 1]);
+		}
+	}
+
+	printf("Отсортированный массив:\n");
+	for (int i = 0; i < 5; i++)
+	{
+		cout << mas[i];
+		printf("\n");
+	}
+}
+
+
